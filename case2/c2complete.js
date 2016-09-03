960713bf7ec7e9846bbcee3e92e82ba4149d31d4
Thomas <thomas.rundle@conduitlearning.com>
2016-09-03 17:50:58 -0500

    Preparing to add subworkflow

--- case2/c2complete.js
+++ case2/c2complete.js
@@ -1,16 +1,9 @@
-// Case 2 | Section 24
-// Objective- 
-//   * Implement serial flow control
-//   * move into modules-
-//   *>> Processing - done
-//   * make functions-
-//   ** Create Consolidated
-//	 ** Pull Log
-//   ** Create Logs
-//   * write JSON file
-//   * use strict
-//   * Put the input filename in the last column of the consolidated csv
+// Case 2
+//TODO
+// - Use validation module
 const fs = require('fs');
+const validate = require('./util/validate');
+
 var workflow = [readDirectory, readFiles, createOutputFile]; //serial workflow
 var settings = {
   input: {
@@ -30,6 +23,8 @@ function readDirectory(config) {
   let path = config.input.path;
   fs.readdir(path, (err, files) => {
     if (err) throw err;
+    console.log('Files in dir: ' + files.length );
+    console.log( files );
     next(null, config, files);
   });
 }
@@ -40,25 +35,27 @@ function readFiles(config, files) {
   let consolidatedData = '';
 
   files.forEach( (file, index, array) => {
-    fs.readFile( path + '/' + file, 'utf8', (err,  data) => { // I'd rather not use the global..folderPath
+    fs.readFile( path + '/' + file, 'utf8', (err,  data) => { 
       if (err) throw err;
-      if (index === 0) {
-        console.log('Files in array: ' + array.length );
-        console.log( array );
-      } else {
+      if (index > 0) {
         data = data.split('\n');
         data.shift();
         data = data.join('\n');
       }
 
       consolidatedData+= data;
+
       supervisor.increment();
-      console.log( supervisor.checkIfComplete() + ' current: ' + supervisor.current );
       if ( supervisor.checkIfComplete() ) {
         next(null, config, consolidatedData);
       }
     });
   });
+}
+// TODO - Move all csv processing into separate function from readFiles
+// * Challenge is managing the subworkflow inside the loop
+function parseCsv(){
+
 }
 
 function createOutputFile(config, data) {
@@ -102,3 +99,14 @@ function next(err, config, result) {
 function execute( config ) {
   next(null, config);
 }
+
+
+// Previous todos
+//   * move into modules-
+//   * make functions-
+//   ** Create Consolidated
+//	 ** Pull Log
+//   ** Create Logs
+//   * write JSON file
+//   * use strict
+//   * Put the input filename in the last column of the consolidated csv
