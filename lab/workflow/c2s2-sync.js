761a8e73990333b5e33decf40991028e095213fb
Thomas <thomas.rundle@conduitlearning.com>
2016-09-09 15:21:23 -0500

    move c2 to workflow lab to test new methods

---
+++ lab/workflow/c2s2-sync.js
@@ -0,0 +1,88 @@
+// Case 2 
+const fs = require('fs');
+const path = require('path');
+console.time('workflow');
+var workflow = [readDirectory, readFiles, createOutputFile]; //serial workflow
+var settings = {
+  input: {
+    path: '_source-data',
+    format: 'csv'
+  },
+  output: {
+    path: 'output',
+    file: 'consolidated.csv'
+  }
+};
+execute(settings);
+// Workflow 
+// * readDirectory
+// * readFiles
+// * createOutputFile
+function readDirectory(config) {
+  let path = config.input.path;
+  let files = fs.readdirSync(path);
+  console.log('Files: ' + files.length);
+  console.log(files);
+  next(null, config, files);
+}
+
+function readFiles(config, files) {
+  let path = config.input.path;
+  let consolidatedData = '';
+
+  files.forEach( (file, index, array) => {
+    var data = fs.readFileSync(path + '/' + file, 'utf8');
+    // sub workflow
+    data = parseCsv(data);   // input: string | output: array
+    data = prepareCsv(data); // input: array  | output: string
+
+    consolidatedData+= data;
+  });
+
+  next(null, config, consolidatedData);
+}
+
+function createOutputFile(config, data) {
+  let path = config.output.path;
+  let file = config.output.file;
+
+  fs.writeFileSync(path + '/' + file, data);
+  console.log('File written to: ' + path + '/' + file);
+  next(null, config);
+}
+
+// Workflow management
+function next(err, config, result) {
+  if (err) throw err;
+  var currentTask = workflow.shift();
+  if (currentTask) {
+    currentTask(config, result);
+  } else {
+    console.timeEnd('workflow');
+  }
+}
+
+function execute( config ) {
+  next(null, config);
+}
+
+// Sub workflow inside iterator
+// * function parse - identify input format, select appropriate parse
+function parse(config, d){
+  let format = config.input.format;
+  console.log('Input format is: ' + config.input.format);
+  console.log('File: ' + f);
+  if (format === 'csv') {
+    parseCsv(config, d);
+  } else {
+    console.log('Format: ' + format + ' not recognized');
+  }
+}
+function parseCsv(d) {
+    d = d.split('\n');
+    d.shift();
+    return d;
+}
+function prepareCsv(d) {
+    return d = d.join('\n');
+}
